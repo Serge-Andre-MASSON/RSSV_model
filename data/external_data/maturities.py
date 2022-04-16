988ee6996a6a374e621333a67c3d75198e169f61
Serge <serge.andre.masson@gmail.com>
2022-04-16 07:19:20 +0200

    Update yahoo and maturity .py

--- data/external_data/maturities.py
+++ data/external_data/maturities.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import numpy as np
 
 
-def convert_maturity(maturity: str, today: str = None) -> float:
+def convert_maturity(maturity: str, today: str = None, round: int = 3) -> float:
     """Convert the given maturity, formatted as "%Y-%m-%d", into the the number of day between now (or "today") and this maturity, normalized as 1 = 365.
 
     Args:
@@ -26,6 +26,6 @@ def convert_maturity(maturity: str, today: str = None) -> float:
     # Return it as a number of day when one year is one
     converted_maturity = (maturity_datetime - today_datetime).days/365
     if converted_maturity > 0:
-        return converted_maturity
+        return np.round(converted_maturity, 3)
     else:
         raise ValueError("Maturity is less than one day from now.")
--- data/external_data/yahoo.py
+++ data/external_data/yahoo.py
@@ -1,25 +1,47 @@
 from datetime import datetime
 from functools import cache
 import re
+from typing import Callable
 import pandas as pd
 from yfinance.ticker import Ticker
+
+# TODO : Réécrire et simplifier ça. Tout ce dont j'ai besoin c'est d'un array/dataframe contenant T, K, C/P, volume?, impliedVol?
 
 
 class Yahoo():
     def __init__(self, asset_name: str):
         self.ticker = Ticker(asset_name)
-        self.maturities = self.ticker.options
-        self.current_asset_price = self.ticker.info['currentPrice']
-        self.call_options_data = self.call_options()
-        self.put_options_data = self.put_options()
 
-    @cache
-    def options_at(self, maturity):
-        """Return a dataframe containing options data for this maturity."""
-        return self.ticker.option_chain(maturity)
+    @property
+    def current_asset_price(self):
+        return self.ticker.info['currentPrice']
 
-    def call_options(self):
-        return {maturity: self.options_at(maturity).calls for maturity in self.maturities}
+    @property
+    def maturities(self):
+        return self.ticker.options
 
-    def put_options(self):
-        return {maturity: self.options_at(maturity).puts for maturity in self.maturities}
+    def options(self, option_type="call"):
+        if not option_type in ["call", "put"]:
+            raise ValueError("option_type must be 'call' or 'put'")
+
+        maturities = self.maturities
+        options_df: pd.DataFrame = None
+
+        for maturity in maturities:
+            if options_df is None:
+                options_df = self.calls(
+                    maturity) if option_type == "call" else self.puts(maturity)
+                options_df['maturity'] = maturity
+            else:
+                options = self.calls(
+                    maturity) if option_type == "call" else self.puts(maturity)
+                options['maturity'] = maturity
+                options_df = pd.concat(
+                    [options_df, options], ignore_index=True)
+        return options_df
+
+    def calls(self, maturity):
+        return self.ticker.option_chain(maturity).calls
+
+    def puts(self, maturity):
+        return self.ticker.option_chain(maturity).puts
